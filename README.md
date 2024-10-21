045cf652b46fe4c919bb45cf6989a404dc699972
yuwnloy <yuwnloyblog@163.com>
2024-10-21 11:08:03 +0800

    add example

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # imserver-sdk-java
 The Java SDK for JIM API
+
+# example
+
+```java
+
+package com.juggle.im;
+
+import com.juggle.im.shadow.RcJuggleIm;
+
+import io.rong.models.response.TokenResult;
+import io.rong.models.user.UserModel;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        RcJuggleIm sdk = new RcJuggleIm("appkey", "appsecret", "http://127.0.0.1:8082");
+        try {
+            TokenResult result = sdk.user.register(new UserModel("userid1", "user1", ""));
+
+           System.out.println(result.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+```
--- main/java/com/juggle/im/App.java
+++ main/java/com/juggle/im/App.java
@@ -1,8 +1,9 @@
 package com.juggle.im;
 
-import com.juggle.im.models.user.UserInfo;
-import com.juggle.im.models.user.UserInfoResult;
-import com.juggle.im.models.user.UserTokenResult;
+import com.juggle.im.shadow.RcJuggleIm;
+
+import io.rong.models.response.TokenResult;
+import io.rong.models.user.UserModel;
 
 /**
  * Hello world!
@@ -12,16 +13,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        JuggleIm sdk = new JuggleIm("appkey", "appsecret", "http://127.0.0.1:8082");
+        RcJuggleIm sdk = new RcJuggleIm("appkey", "appsecret", "http://127.0.0.1:8082");
         try {
-           UserTokenResult result = sdk.user.register(new UserInfo("userid1", "user1", ""));
+            TokenResult result = sdk.user.register(new UserModel("userid1", "user1", ""));
 
            System.out.println(result.toString());
-           System.out.println(result.getUserToken().getToken());
-
-           UserInfoResult uResult = sdk.user.get("userId1");
-           System.out.println(uResult.toString());
-           System.out.println(uResult.getUserInfo().getNickname());
         } catch (Exception e) {
             e.printStackTrace();
         }
